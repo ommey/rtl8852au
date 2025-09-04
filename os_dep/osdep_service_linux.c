169bc9c11218c18611e70543e72ed07bdcd342fb
ommey <omar.aayoubii@gmail.com>
2025-09-04 21:48:59 +0200

    removed some deprecated kernel calls

--- os_dep/osdep_service_linux.c
+++ os_dep/osdep_service_linux.c
@@ -390,8 +390,8 @@ static int openFile(struct file **fpp, const char *path, int flag, int mode)
 	struct file *fp;
 
 #if defined(MODULE_IMPORT_NS)
-	MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
-#endif
+//	MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+ #endif
 
 	fp = filp_open(path, flag, mode);
 	if (IS_ERR(fp)) {
@@ -508,7 +508,7 @@ static int isFileReadable(const char *path, u32 *sz)
 	char buf;
 
 #if defined(MODULE_IMPORT_NS)
-	MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+//	MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
 #endif
 
 	fp = filp_open(path, O_RDONLY, 0);
